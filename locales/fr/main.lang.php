fbc97dd6ac06bb053393c0ba6fe8b0b09bce057b
Pierre <brol31@gmail.com>
2023-01-11 12:59:39 +0100

    localisation et changelog

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,63 @@
+<?php
+// Language: Français
+// Module: multiToc - 
+// Date: 2023-01-11 11:00:01
+// Author: Pierre Van Glabeke
+// Translated with dcTranslater - 2022.12.26
+
+
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+l10n::$locales['Tables of content'] = 'Tables des matières';
+l10n::$locales['Table of content'] = 'Table des matières';
+l10n::$locales['Add post TOC'] = 'Ajouter une table des matières à un billet';
+l10n::$locales['By category'] = 'Par catégorie';
+l10n::$locales['By tag'] = 'Par mot-clé';
+l10n::$locales['By alpha order'] = 'Par ordre alphabétique';
+l10n::$locales['MultiToc: table of content'] = 'MultiToc : table des matières';
+l10n::$locales['Contents by category, keyword and alphabetical order'] = 'Table des matières par catégorie, mot-clé et ordre alphabétique';
+l10n::$locales['Table of content by category'] = 'Table des matières par catégorie';
+l10n::$locales['Table of content by alpha list'] = 'Table des matières par ordre alphabétique';
+l10n::$locales['Table of content by tag'] = 'Table des matières par mot-clé';
+l10n::$locales['Click on each group name to display or hide entries list'] = 'Cliquez sur le nom de chacun des groupes pour afficher ou cacher la liste des billets';
+l10n::$locales['Title up'] = 'Titres croissants';
+l10n::$locales['Title down'] = 'Titres décroissants';
+l10n::$locales['Date up'] = 'Dates croissantes';
+l10n::$locales['Date down'] = 'Dates décroissantes';
+l10n::$locales['Author up'] = 'Auteurs croissants';
+l10n::$locales['Author down'] = 'Auteurs décroissants';
+l10n::$locales['Comments number up'] = 'Nombre de commentaires croissant';
+l10n::$locales['Comments number down'] = 'Nombre de commentaires décroissant';
+l10n::$locales['Trackbacks number up'] = 'Nombre de rétroliens croissant';
+l10n::$locales['Trackbacks number down'] = 'Nombre de rétroliens décroissant';
+l10n::$locales['TOC by tags'] = 'Table des matières par mot-clé';
+l10n::$locales['Enable TOC by tags'] = 'Activer la table des matières par mot-clé';
+l10n::$locales['Order of tags:'] = 'Ordre des mots-clés :';
+l10n::$locales['Order of entries:'] = 'Ordre des billets :';
+l10n::$locales['Name up'] = 'Noms croissants';
+l10n::$locales['Name down'] = 'Noms décroissants';
+l10n::$locales['TOC by alpha list'] = 'Tables des matières par lettre';
+l10n::$locales['Enable TOC by alpha list'] = 'Activer la table des matières par lettre';
+l10n::$locales['Order of alpha list:'] = 'Ordre de la liste des lettres :';
+l10n::$locales['Alpha up'] = 'Lettres croissantes';
+l10n::$locales['Alpha down'] = 'Lettres décroissantes';
+l10n::$locales['Post TOC'] = 'Table des matières pour les billets';
+l10n::$locales['Enable post TOC'] = 'Activer la table des matières pour les billets';
+l10n::$locales['Auto numbering'] = 'Numérotation automatique';
+l10n::$locales['TOC by category'] = 'Tables des matières par catégorie';
+l10n::$locales['Enable TOC by category'] = 'Activer la table des matières par catégorie';
+l10n::$locales['Order of categories:'] = 'Ordre des catégories :';
+l10n::$locales['No option'] = 'Aucune option';
+l10n::$locales['Display entry number of each group'] = 'Afficher le nombre de billets pour chaque groupe';
+l10n::$locales['Display date'] = 'Afficher la date';
+l10n::$locales['Display author'] = 'Afficher l\'auteur';
+l10n::$locales['Display category'] = 'Afficher la catégorie';
+l10n::$locales['Display comment number'] = 'Afficher le nombre de commentaires';
+l10n::$locales['Display trackback number'] = 'Afficher le nombre de rétroliens';
+l10n::$locales['Display tags'] = 'Afficher les mots-clés';
+l10n::$locales['Format date:'] = 'Format de la date :';
+l10n::$locales['Those options require stacker plugin'] = 'Ces options requièrent le plugin stacker';
+l10n::$locales['Configuration has been saved successfully'] = 'La configuration a été enregistrée avec succès';
+l10n::$locales['Makes posts or content\'s tables of content'] = 'Créer des tables des matières pour une liste de billets ou le contenu d\'un billet';
